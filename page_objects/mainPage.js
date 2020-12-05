95308e2273a26a9dcd06049603be64e845658f56
Jorge Carrillo <e.carrillo.cirerol@gmail.com>
2020-12-04 19:00:14 -0600

    Fixed script after most recent todoist update

--- page_objects/mainPage.js
+++ page_objects/mainPage.js
@@ -2,20 +2,20 @@ import { Selector, t } from 'testcafe'
 
 class MainPage {
   constructor () {
-    this.settingsGear = Selector('.gear_normal')
-    this.logoutButton = Selector('.usermenu__row-label').withText('Cerrar sesión')
+    this.settingsAvatar = Selector('.user_avatar.big.settings_avatar')
+    this.logoutButton = Selector('.user_menu_label').withText('Cerrar sesión')
     this.plusAddButton = Selector('.plus_add_button')
     this.taskTextInput = Selector('.notranslate.public-DraftEditor-content')
     this.addTaskButton = Selector('.ist_button.ist_button_red')
-    this.taskDescription = Selector('.items.root_item_list li div div div div')
+    this.taskDescription = Selector('.markdown_content.task_content')
     this.cancelButton = Selector('.cancel')
-    this.closeTaskCheckbox = Selector('.items.root_item_list li div button div.task_checkbox__circle')
+    this.closeTaskCheckbox = Selector('.task_checkbox__circle')
   }
 
   async logout () {
     await t
       .wait(2500)
-      .click(this.settingsGear)
+      .click(this.settingsAvatar)
       .click(this.logoutButton)
   }
 
